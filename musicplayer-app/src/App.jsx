65e3d0ceeee0e04282e94cfb25865579b8abf19e
Hopizy <147936629+Hopizy@users.noreply.github.com>
2025-09-07 06:58:30 +0100

    Update App.jsx

--- musicplayer-app/src/App.jsx
+++ musicplayer-app/src/App.jsx
@@ -9,17 +9,18 @@ import Contact from './pages/Contact'
 export default function App() {
 
   return (
-    <div className="min-h-screen bg-gray-50 pb-28 bg-[url('/backimage.jpg')]bg-cover bg-center">
+    <div className="min-h-screen bg-gray-50 pb-28 bg-[url('/backimage.jpg')]">
       {/* Header with navigation */}
       <header className="z-10 bg-red border-b">
         <div className="max-w-5xl mx-auto px-4 py-4 flex items-center gap-6">
           <nav className="flex gap-4 text-sm text-gray-600">
             <h2 className="text-xl md:text-3xl lg:text-4xl font-extrabold">🎵MusicPlayerGallery</h2>
-            <Link to="/" className="hover:text-indigo-600">Home</Link>
-            <Link to="/about" className="hover:text-indigo-600">About</Link>
-            <Link to="/login" clasName="hover:text-indigo-600">Login</Link>
+            <Link to="/" className="hover:text-yellow-200">Home</Link>
+            <Link to="/about" className="hover:text-yellow-200">About</Link>
+            <Link to="/login" clasName="hover:text-yellow-200">Login</Link>
             <Link to="/contact" className="hover:text-indigo-600">Contact</Link>
           </nav>
+          <button className="sm:hidden text-white">☰</button>
         </div>
       </header>
     
